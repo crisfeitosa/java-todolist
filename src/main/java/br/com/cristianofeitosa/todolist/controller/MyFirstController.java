b2566bb6af635c93f4442df62333a8567e4b8423
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-11 10:30:24 -0300

    Add MyFirstController with a simple Hello World endpoint

---
+++ src/main/java/br/com/cristianofeitosa/todolist/controller/MyFirstController.java
@@ -0,0 +1,15 @@
+package br.com.cristianofeitosa.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firstRouter")
+public class MyFirstController {
+  
+  @GetMapping("/")
+  public String firstMessage() {
+    return "Hello World";
+  }
+}

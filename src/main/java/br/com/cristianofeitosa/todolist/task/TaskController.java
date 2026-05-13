0ee1f20366a9f966fae17ca5cec1dcb3039783d5
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-13 13:01:31 -0300

    Enhance update method in TaskController to handle task not found and user permission checks

--- src/main/java/br/com/cristianofeitosa/todolist/task/TaskController.java
+++ src/main/java/br/com/cristianofeitosa/todolist/task/TaskController.java
@@ -54,11 +54,24 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Tarefa não encontrada");
+    }
+
+    var userId = request.getAttribute("userId");
+
+    if (!task.getUserId().equals(userId)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Usuario não tem permissão para atualizar essa tarefa");
+    }
 
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
   }
 }
